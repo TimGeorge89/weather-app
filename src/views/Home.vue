7bf75761ceb5f7dd34ee325b0faad8824f1242b5
Tim <Fallenpyr@aol.com>
2020-03-09 11:28:12 -0400

    UI updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="home">
     <div class="container">
+      <!-- FRONT PAGE / INPUT ZIP -->
       <div v-if="Object.keys(data).length == 0" class="info">
         <h1>Weather App</h1>
-        <p>Get your local wether</p>
-        <input v-model="searchValue" @keydown.enter="fetchWeather()" type="text" placeholder="search by zip">
-        <button @click="fetchWeather()" >Search</button>
+        <p>Get your local weather</p>
+        <input v-model="searchValue" @keydown.enter="fetchWeather()" type="text" placeholder="search by zip or city">
+        <button @click="fetchWeather()">Search</button>
         <p v-if="showResultsError" class="error">There were no results, try a differnt zip.</p>
       </div>
+      <!-- RESULTS -->
       <div v-else class="results">
-        <p>the weather is: {{ Math.ceil(data.main.temp) }} degrees</p>
+        <div class="weather-img">
+          <img :src="`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`" />
+        </div>
+        <h5>{{ (data.weather[0].description) }} in your area.</h5>
+        <p>{{ data.name }}</p>
+        <p class="temperature"> {{ Math.ceil(data.main.temp) }}°</p>
         <button @click="data = {}"> new location</button>
       </div>
     </div>
@@ -35,23 +42,30 @@
       }
     },
     methods: {
+      // FETCH API DATA
       async fetchWeather() {
         try {
           const apiKey = 'a38a8ac08913133d21521e824b908cb5'
-          const api = `https://api.openweathermap.org/data/2.5/weather?zip=${this.searchValue},us&units=imperial&appid=${apiKey}`
+          let api = `https://api.openweathermap.org/data/2.5/weather?zip=${this.searchValue},us&units=imperial&appid=${apiKey}`
 
+          // USE CITY INSTEAD OF ZIP
+          if (isNaN(this.searchValue)) {
+            api = `https://api.openweathermap.org/data/2.5/weather?q=${this.searchValue},us&units=imperial&appid=${apiKey}`
+          }
+
+          // AWAIT RESPONSE
           let res = await fetch(api)
           let data = await res.json()
-
+          // IF THERE IS DATA
           if (data.main) {
             this.data = data
           } else {
             this.showResultsError = true
           }
-
+          // RESET SEARCH FEILD
           this.searchValue = ''
+
         } catch (err) {
-          console.log(err);
           alert('network error, try again')
         }
       }
@@ -100,10 +114,15 @@
   }
 
   .results {
-    padding: 20px 0;
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    padding: 15px 0;
 
-    p {
-
+    .temperature {
+        font-size: 45px;
     }
+
+    h4 {}
   }
 </style>
